e921e7dda2bdd4256743ba75787ffc9770c996c2
demo1 <tanujarbhong@gmail.com>
2024-03-23 18:15:51 +0530

    Initial commit

---
+++ pro1/app2/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+
+def login_view(request):
+    template_name = 'app2/login.html'
+    if request.method == 'POST':
+        un = request.POST['un']
+        pw = request.POST['pw']
+        user = authenticate(username=un, password=pw)
+        if user:
+            login(request, user)
+            return redirect('show_url')
+        else:
+            return HttpResponse('INCORRECT CREDENTIALS')
+    return render(request, template_name)
+
+
+@login_required(login_url="login_url")
+def logout_view(request):
+    logout(request)
+    return redirect('signup_url')
+
+
+def signup_view(request):
+    template_name = 'app2/register.html'
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('show_url')
+    context = {'form': form}
+    return render(request, template_name, context)
+
+
+
+
+@login_required(login_url="login_url")
+def cpass_view(request):
+    pass
